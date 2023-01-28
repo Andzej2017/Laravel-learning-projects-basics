2ff429056d4172cae6560fa2777f16ff05bb7a26
Andzej2017 <andrzej.j2007@yahoo.com>
2023-01-28 18:27:55 +0200

    Basic project, basic routing

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//Laravel 9
+Route::controller(DemoController::class)->group(function(){
+    Route::get('/about', 'Index')->name('about.page');
+    Route::get('/contact', 'ContactMethod')->name('contact.page');
+});
+
+//Older
+//Route::get('/about', [DemoController::class,'Index']);
+//Route::get('/contact', [DemoController::class,'ContactMethod']);
