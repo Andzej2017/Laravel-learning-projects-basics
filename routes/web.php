b3902464188351f216f02244d876d52c4f7ea306
Andzej2017 <andrzej.j2007@yahoo.com>
2023-01-29 18:15:01 +0200

    breeze install

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Demo\DemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +17,15 @@ use App\Http\Controllers\Demo\DemoController;
 Route::get('/', function () {
     return view('welcome');
 });
-//Laravel 9
-Route::controller(DemoController::class)->group(function(){
-    Route::get('/about', 'Index')->name('about.page')->middleware('check');
-    Route::get('/contact', 'ContactMethod')->name('contact.page');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//Older
-//Route::get('/about', [DemoController::class,'Index']);
-//Route::get('/contact', [DemoController::class,'ContactMethod']);
+require __DIR__.'/auth.php';
